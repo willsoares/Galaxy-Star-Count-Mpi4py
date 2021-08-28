27dd533c7dbd054308691bcba9a2aa6f7a641ab2
willsoares <willsoares96@hotmail.com>
2021-08-28 18:25:16 -0300

    update to python3

--- start_count.py
+++ start_count.py
@@ -5,7 +5,7 @@
 ################################################################################################################
 
 import numpy as np
-import Image
+from PIL import Image
 import cv2
 from mpi4py import MPI
 
@@ -13,44 +13,45 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-c = np.array([0])
+c = np.array([0], dtype=np.uint8)
 local_x = np.array([0])
-a = np.array((12788,40000),dtype='uint8') # Image has 12788 rows and 40000 colums
+# Image has 12788 rows and 40000 colums
+a = np.array((12788, 40000), dtype='uint8')
 
 if rank == 0:
-        t1 = MPI.Wtime()
-        img = cv2.imread("heic1502a.tif",0) #Read the image
-        t2 = MPI.Wtime()
-        print " Time taken to open and read the image is : %r sec " %(t2-t1)
-        a = np.array(img)  #Convert to a Matrix
+    t1 = MPI.Wtime()
+    img = cv2.imread("heic1502a.tif", 0)  # Read the image
+    t2 = MPI.Wtime()
+    print(" Time taken to open and read the image is : %r sec " % (t2-t1))
+    a = np.array(img)  # Convert to a Matrix
 
 w1 = MPI.Wtime()
 remender = 12788 % size  # devide image horizontally
 
 if rank < remender:
-        rowsize = 12788/size
-        rowsize = rowsize + 1
+    rowsize = 12788/size
+    rowsize = rowsize + 1
 else:
-        rowsize = 12788/size
+    rowsize = 12788/size
 
-c = np.array((rowsize,40000)) # Part of image given to each process
+c = np.array((rowsize, 40000), dtype=np.uint8)  # Part of image given to each process
 comm.Bcast(c, root=0)
-local_x = np.zeros(c,dtype='uint8')
+local_x = np.zeros(c, dtype=np.uint8)
 comm.Bcast(local_x, root=0)
 total = np.array([0])
 
-comm.Scatterv(a,local_x,root=0) # scatter the image
+comm.Scatterv(a, local_x, root=0)  # scatter the image
 
-#image processing
-img_node_thresh =  cv2.adaptiveThreshold(local_x,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,59,0)
+# image processing
+img_node_thresh = cv2.adaptiveThreshold(
+    local_x, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 59, 0)
 star_count_node = ((200 < img_node_thresh)).sum()
-print " Star count at Rank", rank,"is ", star_count_node
+print(" Star count at Rank", rank, "is ", star_count_node)
 
-comm.Reduce(star_count_node,total,op=MPI.SUM,root=0) # Reduce to zero process
+comm.Reduce(star_count_node, total, op=MPI.SUM,
+            root=0)  # Reduce to zero process
 
 if comm.rank == 0:
-        w2 = MPI.Wtime()
-        print " Total Stars ", total
-        print " Total time taken", w2-w1 ,"sec"
-
-
+    w2 = MPI.Wtime()
+    print(" Total Stars ", total)
+    print(" Total time taken", w2-w1, "sec")
